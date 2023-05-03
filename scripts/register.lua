2c17a56333d5f448095ca0ffbd51674050e18b96
inconspicuously007 <58066449+inconspicuously007@users.noreply.github.com>
2023-05-03 09:03:16 +0200

    Add files via upload
    Fix to prevent loading if main mod is loaded

--- scripts/register.lua
+++ scripts/register.lua
@@ -3,23 +3,6 @@ local modName = g_currentModName or "unknown"
 local function validateTypes(typeManager)
 	if g_modIsLoaded[modName] and typeManager.typeName == "placeable" then		
 		for typeName, typeEntry in pairs(typeManager:getTypes()) do				
-			--if typeName == "silo" then
-			--	typeManager:addSpecialization(typeName, modName .. ".siloDisplays")
-			--end	
-			--if typeName == "productionPoint" then
-			--	typeManager:addSpecialization(typeName, modName .. ".productionPointDisplays")				
-			--end
-			--if typeName == "manureHeap" then
-			--	typeManager:addSpecialization(typeName, modName .. ".manureHeapDisplays")
-			--end	
-			--if typeName == "bunkerSilo" then
-			--	typeManager:addSpecialization(typeName, modName .. ".bunkerSiloDisplays")
-			--end
-			--if typeName == "cowHusbandryFeedingRobot" or typeName == "cowHusbandry" or typeName == "chickenHusbandry" or typeName == "horseHusbandry" or typeName == "pigHusbandry" or typeName == "pigHusbandryFeedingRobot" or typeName == "FS22_Departement_Haut_Beyleron.pigHusbandryFeedingRobot" then				
-			--	typeManager:addSpecialization(typeName, modName .. ".husbandryDisplays")
-			--	printf("typeName: '%s'", typeName)
-			--	DebugUtil.printTableRecursively(typeEntry,"_",0,3)
-			--end
 			if typeEntry.specializationsByName["silo"] ~= nil then
 				typeManager:addSpecialization(typeName, modName .. ".siloDisplays")
 			end
@@ -40,7 +23,11 @@ local function validateTypes(typeManager)
 end
 
 local function init()	
-	TypeManager.validateTypes = Utils.prependedFunction(TypeManager.validateTypes, validateTypes)
+	if g_modIsLoaded.FS22_advancedProductionInfoFunctions then	
+		printf("Found FS22_advancedProductionInfoFunctions. Skipping registration in '%s'.", modName)
+	else
+		TypeManager.validateTypes = Utils.prependedFunction(TypeManager.validateTypes, validateTypes)
+	end	
 end
 
 init()
